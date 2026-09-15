c13d4a9d69ee02c230c32055c20cb9f806bd1977
Eduardo Fritsch Silva <eduardofritschsilva@gmail.com>
2026-09-15 08:51:47 -0300

    Inicialização do projeto

---
+++ src/main/java/com/betman/BetmanApplication.java
@@ -0,0 +1,13 @@
+package com.betman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betman/BetmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
